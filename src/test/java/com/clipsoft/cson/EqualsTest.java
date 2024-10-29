93a7fa1197165f6767417dc0b679545df707e56b
ice3x2 <ice3x2@gmail.com>
2024-10-30 05:48:41 +0900

    test: 타입을 알 수 없는 Number 간 비교

--- src/test/java/com/clipsoft/cson/EqualsTest.java
+++ src/test/java/com/clipsoft/cson/EqualsTest.java
@@ -2,6 +2,9 @@ package com.clipsoft.cson;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
@@ -15,6 +18,17 @@ public class EqualsTest {
         CSONObject v1CSON = new CSONObject(v1);
         CSONObject v2CSON = new CSONObject(v2);
         assertNotEquals(v1CSON, v2CSON);
+
+    }
+
+    @Test
+    public void numberEquals() {
+        CSONElements.equalsNumber(111.0, 111);
+        CSONElements.equalsNumber(111.0000, 111f);
+        CSONElements.equalsNumber(111.0000, 111d);
+        CSONElements.equalsNumber(111.0000, BigInteger.valueOf(111));
+        CSONElements.equalsNumber(111.0000, BigDecimal.valueOf(111));
+        CSONElements.equalsNumber(111.001, BigDecimal.valueOf(111.001));
 
     }
 }
